50078917663e0d7ee4d16bd7eaf11419c8cf47ad
LakshyaRawat <lakshya_rawat@epam.com>
2026-03-24 11:48:27 +0530

    commit 4:anurag_merge conflict

--- Main.java
+++ Main.java
@@ -5,12 +5,9 @@ public class Main {
         System.out.println("this is commit 2:Lakshaya");
         System.out.println("this is commit 3: Lakshaya");
         System.out.println("this is the feature branch modified line");
-        System.out.println("this is the master branch modified line:Lakshya");
-        System.out.println("this is the master branch modified line 2 :Lakshya");
-        System.out.println("this is the master branch modified line 3 :Lakshya");
-        System.out.println("this is the master branch modified line 4 :Lakshya");
-        System.out.println("this is the master branch modified line 5 :Lakshya");
-        System.out.println("this is the master branch modified line 6 :Lakshya");
-    
+        System.out.println("this is the master branch modified line:ANURAG");
+        System.out.println("this is the master branch modified line 2 :ANURAG");
+        System.out.println("this is the master branch modified line 3 :ANURAG");
+        System.out.println("this is the master branch modified line 4 :ANURAG");
     }
 }   

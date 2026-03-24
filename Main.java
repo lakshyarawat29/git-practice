a4838c5f8c1942a325f11f78e1210d3cc7c0c97b
LakshyaRawat <lakshya_rawat@epam.com>
2026-03-24 11:13:46 +0530

    checking merge conflict

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("this is commit 1");
         System.out.println("this is commit 2:Lakshaya");
         System.out.println("this is commit 3: Lakshaya");
+        System.out.println("This will be modified line ");
     }
 }   
